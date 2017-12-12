925855f08ba67d382d18feaf6f53d65b3387fb5c
Steve Huff <shuff@vecna.org>
2017-12-12 17:20:34 -0500

    chore: argh Darwin wtf is bowdlerized

--- spec/bsd_wtf_spec.rb
+++ spec/bsd_wtf_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe BsdWtf do
     expect(BsdWtf::VERSION).not_to be nil
   end
 
-  it "translates wtf" do
-    expect(BsdWtf.wtf("wtf").to_json).to eq("{\"success\":[{\"acronym\":\"WTF\",\"definition\":\"where's the food\"}],\"failure\":[]}")
+  it "translates lol" do
+    expect(BsdWtf.wtf("lol").to_json).to eq("{\"success\":[{\"acronym\":\"LOL\",\"definition\":\"laughing out loud\"}],\"failure\":[]}")
   end
 end
